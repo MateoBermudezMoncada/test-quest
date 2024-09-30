d52a160c3b3b198254f9ee111014e72a0ac4fd7d
Mateo Bermudez <mateobermudezmoncada@gmail.com>
2024-09-29 19:01:31 -0500

    creating repo

---
+++ src/main/java/test_quest/riwi/RiwiApplication.java
@@ -0,0 +1,13 @@
+package test_quest.riwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiwiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiwiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test_quest/riwi/RiwiApplicationTests.java
@@ -0,0 +1,13 @@
+package test_quest.riwi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiwiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
